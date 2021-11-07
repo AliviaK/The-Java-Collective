22a148974a13a08a0d57731d1f735698305a1ac9
Alivia Kleinfeldt <alivia.kleinfeldt@gmail.com>
2021-11-06 22:30:37 -0500

    create class shells

---
+++ src/main/java/edu/matc/controller/ReportsResource.java
@@ -0,0 +1,11 @@
+package edu.matc.controller;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+// Class to be filled out
+// @Path("/reports")
+public class ReportsResource {
+
+}
---
+++ src/main/java/edu/matc/entity/Reports.java
@@ -0,0 +1,5 @@
+package edu.matc.entity;
+
+// Income Data object I named Reports but can be refactored if there is a better name for it
+public class Reports {
+}

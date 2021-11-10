963d17ecd4d2e976c94077407665097a0e49e35a
e-christenson <erikchristenson@gmail.com>
2021-11-09 18:49:04 -0600

    removing notes from zdaotest / possibly changing pom to include junit

--- src/test/java/ZippopotamusDaoTest.java
+++ src/test/java/ZippopotamusDaoTest.java
@@ -7,12 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 
-// this wont compile.  I need to add the actual DAO and classes to support the data (robopojo + jason stuff)
-// but im not able to build out directories / they seem to not push when I commit
-//adding this file to attempt to push dir structure up **also begining of test class
-
-
-
 public class ZippopotamusDaoTest {
     @Test
     public void testZipApi() throws Exception {

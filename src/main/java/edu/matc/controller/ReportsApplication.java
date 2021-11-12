1a9c092b6f69f03922149b946cf070c7953ed2eb
e-christenson <erikchristenson@gmail.com>
2021-11-12 15:08:32 -0600

    working on reportsApp and reports class

--- src/main/java/edu/matc/controller/ReportsApplication.java
+++ src/main/java/edu/matc/controller/ReportsApplication.java
@@ -1,8 +1,18 @@
 package edu.matc.controller;
 
+import Zippopotamus.API.ZipCode;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import edu.matc.entity.Reports;
+import edu.matc.persistence.ZippopotamusDAO;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 //Defines the base URI for all resource URIs.
@@ -18,4 +28,58 @@ public class ReportsApplication extends Application {
         // h.add(ReportsResource.class );
         return h;
     }
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    //method takes zipcode and collects data from api / db
+    //data  is then set into Reports object
+
+    public List<Reports> processZipCode(int zipCodeToProcess, List<Integer> yearsToReturn) throws JsonProcessingException {
+        ZippopotamusDAO zippoDAO = new ZippopotamusDAO();
+        List<Reports> reports = new ArrayList<>();
+        Reports report = null;
+        //setting up the object with API call, zc will contain city/state data
+        ZipCode zc = zippoDAO.GetCityState(String.valueOf(zipCodeToProcess));
+
+        //loop through years to setup Reports object
+
+        int numOfYears = yearsToReturn.size();
+        int i = 0;
+
+        logger.info("Years (how many) sent to processZipCode: "+numOfYears);
+        logger.info("Years 0: "+yearsToReturn.get(0));
+        logger.info("Years 1 "+yearsToReturn.get(1));
+        logger.info("Years2: "+yearsToReturn.get(2));
+
+        while (i<numOfYears) {
+            report = new Reports();
+            report.setZipCode(zipCodeToProcess);
+            report.setCity(zc.getPlaces().get(0).getPlaceName());
+            report.setState(zc.getPlaces().get(0).getState());
+            report.setYear(yearsToReturn.get(i));
+            // reports.setHouseholdMedianIncome();
+
+            reports.add(report);
+            logger.info("addad a new report: "+report);
+            i++;
+        }
+
+        logger.info("report 0: "+reports.get(0));
+        logger.info("r 1 "+reports.get(1));
+        logger.info("r 2 : "+reports.get(2));
+
+
+        return reports;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }

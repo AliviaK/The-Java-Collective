ad4742155ea00825f40bf73a8592651e1585c501
Alivia Kleinfeldt <alivia.kleinfeldt@gmail.com>
2021-11-15 22:16:21 -0600

    delete unused genericDAO

--- src/test/java/edu/matc/entity/IncomeDataTest.java
+++ src/test/java/edu/matc/entity/IncomeDataTest.java
@@ -3,6 +3,7 @@ package edu.matc.entity;
 import edu.matc.persistence.IncomeDataDAO;
 import org.junit.jupiter.api.BeforeEach;
 
+// Did I make this? TODO: delete?
 class IncomeDataTest {
 
     IncomeDataDAO incomeDataDAO;

9c35437e6cfcebaf37c7d96a3aef92550e4ffa3a
e-christenson <erikchristenson@gmail.com>
2021-11-16 20:52:57 -0600

    refactored "reportsapplicationTest" to processReportsTest, and fixed tests inside to only call one year (vs multiple)

--- src/test/java/ProcessReportsTest.java
+++ src/test/java/ProcessReportsTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 
-public class ReportsApplicationTest {
+public class ProcessReportsTest {
 
     @Test
     public void testProcessZipCode() throws Exception {
